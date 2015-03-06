92492a24175ce71570dceb1aa68f3c6661cbae63
wallas4 <wallas4@unlv.nevada.edu>
2015-03-06 20:47:53 +0000

    CS 270 Homework 4

---
+++ db/seeds.rb
@@ -0,0 +1,20 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+2.times do |i|
+  Event.create(name: "Event #{i + 1}")
+end
+
+5.times do |i|
+    Location.create(
+        name: "Location #{i + 1}",
+        tag: (('A'..'Z').to_a + ('a'..'z').to_a + (0..9).to_a).shuffle[0..7].join,
+    )
+end
